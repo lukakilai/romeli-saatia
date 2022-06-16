5019de8d6354a8e40be631c093dddf44f904e212
Luka Kilaberidze <lkila16@freeuni.edu.ge>
2022-06-17 00:27:20 +0400

    chore: extract hour, minute, ampm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import moment from "moment";
+
 export default function App() {
+  const time = moment().format("h:mm:A");
+  const [hour, minute, ampm] = time.split(":");
+
+  console.log("hour", hour);
+  console.log("minute", minute);
+  console.log("ampm", ampm);
+
   return (
-    <div className="App">
-      <p className="text-7xl font-bod">Hello</p>
+    <div className="w-full h-screen grid place-items-center">
+      <p className="text-4xl font-bold">{time}</p>
     </div>
   );
 }
